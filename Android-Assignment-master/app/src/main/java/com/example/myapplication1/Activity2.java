820aaa950c64d76fb26b1c951479e40aa80bd604
harshwardhan5301 <84539191+harshwardhan5301@users.noreply.github.com>
2021-06-04 20:29:06 +0530

    Add files via upload

---
+++ Android-Assignment-master/app/src/main/java/com/example/myapplication1/Activity2.java
@@ -0,0 +1,43 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sample_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.item1)
+        {
+            Intent intent = new Intent(Activity2.this,Activity1.class);
+            startActivity(intent);
+            return true;
+        }
+        else if(id == R.id.item2){
+            Intent intent = new Intent(Activity2.this,Activity2.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
